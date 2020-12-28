1350801fa52db21475d45186063fc037b30b4605
Salem Eid <salem.eid@gmail.com>
2020-12-28 10:27:56 +0300

    finished the UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import './App.css';
 import Navigation from './components/navigation/Navigation';
+import Logo from './components/logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 import 'tachyons';
 
+const particlesOptions = {
+    particles: {
+      number: {
+        value: 100,
+        density : {
+          enable: true,
+          value_area:800
+        }
+      }
+    }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'params={particlesOptions}
+              />
       <Navigation />
-      {/* <Logo />
+      <Logo />
+      <Rank />
       <ImageLinkForm />
-      <FaceRecongnition /> */}
+      
+      {/* <FaceRecongnition /> */}
 
     </div>
   );
